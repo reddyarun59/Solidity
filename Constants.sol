650fbf2deb71b0a832343335aa0cf336583436c8
reddyarun59 <reddyarun59@yahoo.com>
2022-07-25 16:03:31 +0530

    Constants

---
+++ Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x4Fbc59aAd52577A898edEF895c44b56052ACe823;
+    uint public constant MY_INT = 123;
+}
+ 
+// its better to use constants to reduce gas
